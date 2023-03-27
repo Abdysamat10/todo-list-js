73adc6697f81cf3436062414a94be6ac846de5fd
samat <samat020404@gmail.com>
2023-03-28 01:06:04 +0800

    gradient

--- js/script.js
+++ js/script.js
@@ -40,3 +40,22 @@ function onFormSubmit(event) {
         } else alert('Длина названия должна быть больше 3 и меньше 20');
     }
 }
+
+let colors = [
+    'linear-gradient(90deg, #032e42,#045b64,#de8d56,#f5a815)',
+    'linear-gradient(90deg, #282f33,#62765b,#b98b41,#b03eee,#523caa)',
+    'linear-gradient(90deg, #282f33,#62765b,#b98b41,#b03eee,#523caa)',
+    'linear-gradient(90deg, #1e00ff,#24bdff,#ffffff,#ff7b00,#ff0000)',
+    'linear-gradient(90deg, #fefec0,#f68dae,#dac2f2)',
+];
+let background = document.querySelector('body');
+let changeColors = 0;
+
+function changeColor() {
+    background.style.background = colors[changeColors];
+    changeColors++;
+    if (changeColors >= colors.length) changeColors = 0;
+}
+
+setInterval(changeColor, 10);
+
